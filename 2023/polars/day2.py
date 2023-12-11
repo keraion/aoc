1b5c3badafaeddb43ea1ec6524a025c53c5dd30d
Cameron Loos <105471409+keraion@users.noreply.github.com>
2023-12-10 23:48:06 -0500

    2023 Day 2 - polars

---
+++ 2023/polars/day2.py
@@ -0,0 +1,58 @@
+import polars as pl
+
+df = (
+    pl.read_csv("2023/day2.txt", has_header=False, separator=":")
+    .with_columns(
+        pl.col("column_2")
+        .str.strip_chars()
+        .str.split(";")
+        .explode()
+        .str.strip_chars()
+        .str.split(",")
+        .explode()
+        .str.strip_chars()
+        .str.split(" ")
+        .list.to_struct(fields=["amount", "color"])
+        .implode()
+        .over("column_2"),
+    )
+    .explode("column_2")
+    .unnest("column_2")
+)
+
+print(
+    "Part 1:",
+    df.filter(
+        ~pl.col("column_1").is_in(
+            df.filter(
+                pl.any_horizontal(
+                    (
+                        (pl.col("color") == pl.lit("red"))
+                        & (pl.col("amount").cast(pl.Int64) > 12)
+                    ),
+                    (
+                        (pl.col("color") == pl.lit("green"))
+                        & (pl.col("amount").cast(pl.Int64) > 13)
+                    ),
+                    (
+                        (pl.col("color") == pl.lit("blue"))
+                        & (pl.col("amount").cast(pl.Int64) > 14)
+                    ),
+                )
+            ).to_series()
+        )
+    )
+    .select(pl.col("column_1").str.extract(r"(\d+)").cast(pl.Int64))
+    .unique()
+    .sum(),
+)
+
+print(
+    "Part 2:",
+    df.group_by("column_1", "color")
+    .agg(pl.col("amount").cast(pl.Int64).max())
+    .group_by("column_1")
+    .agg(pl.col("amount").product())
+    .select("amount")
+    .sum(),
+)
