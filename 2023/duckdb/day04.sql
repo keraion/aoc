5c33a55864fbf496fa7437bb56ecfcf8e6a2043e
Cameron Loos <105471409+keraion@users.noreply.github.com>
2023-12-10 23:48:06 -0500

    2023 Day 4 - Duckdb/Polars

---
+++ 2023/duckdb/day04.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE TABLE input as FROM read_csv('2023/day04.txt', columns = {'input_row': 'VARCHAR'}, delim ='~');
+
+CREATE OR REPLACE TABLE base AS
+SELECT
+    regexp_extract_all(string_split(input_row, ':')[1], '\d+')[
+        1
+    ]::int AS card_num,
+    unnest(list_concat(range(
+        regexp_extract_all(string_split(input_row, ':')[1], '\d+')[1]::int + 1,
+        regexp_extract_all(string_split(input_row, ':')[1], '\d+')[1]::int + 1
+        + len(
+            list_intersect(
+                regexp_extract_all(
+                    string_split(string_split(input_row, ':')[2], '|')[1], '\d+'
+                ),
+                regexp_extract_all(
+                    string_split(string_split(input_row, ':')[2], '|')[2], '\d+'
+                )
+            )
+        )
+    ), [NULL])) AS next_card
+FROM input;
+
+WITH RECURSIVE looper AS (
+    SELECT
+        card_num,
+        next_card
+    FROM base
+
+    UNION ALL
+
+    SELECT
+        n.card_num,
+        n.next_card
+    FROM looper AS l
+    INNER JOIN base AS n
+        ON
+            l.next_card = n.card_num
+            AND n.next_card IS NOT NULL
+)
+
+SELECT
+    'Part 1' AS part,
+    sum((2 ** (len(list_intersect(
+        regexp_extract_all(
+            string_split(string_split(input_row, ':')[2], '|')[1], '\d+'
+        ),
+        regexp_extract_all(
+            string_split(string_split(input_row, ':')[2], '|')[2], '\d+'
+        )
+    )) - 1))::int) AS answer
+FROM input
+
+UNION ALL
+
+SELECT
+    'Part 2' AS part,
+    count(*) AS answer
+FROM looper;
---
+++ 2023/polars/day04.py
@@ -0,0 +1,61 @@
+import polars as pl
+
+df = pl.read_csv("2023/day04.txt", has_header=False, separator=":").with_columns(
+    pl.col("column_1").str.extract(r"(\d+)").cast(pl.Int64).alias("card_num"),
+    pl.col("column_2")
+    .str.split("|")
+    .list.first()
+    .str.extract_all(r"\d+")
+    .alias("winners"),
+    pl.col("column_2")
+    .str.split("|")
+    .list.last()
+    .str.extract_all(r"\d+")
+    .alias("selected"),
+)
+
+df_next_cards = df.select(
+    pl.col("card_num"),
+    pl.int_ranges(
+        pl.col("card_num") + 1,
+        pl.col("card_num")
+        + 1
+        + pl.col("winners").list.set_intersection(pl.col("selected")).list.len(),
+    ).alias("next_card"),
+).explode("next_card")
+
+df_next_cards = df_next_cards.with_columns(pl.col("next_card").alias("next_card_0"))
+df_base = df_next_cards.with_columns(
+    (
+        pl.col("card_num").unique().count() + pl.col("next_card").drop_nulls().count()
+    ).alias("hit_count")
+)
+
+n = 0
+while len(df_base.drop_nulls(f"next_card_{n}")) > 0:
+    df_base = df_base.join(
+        df_next_cards,
+        how="left",
+        left_on=f"next_card_{n}",
+        right_on="card_num",
+        suffix=f"_{n+1}",
+    ).with_columns(
+        pl.col("hit_count") + pl.col(f"next_card_{n+1}").drop_nulls().count()
+    )
+
+    n += 1
+
+print(
+    "Part 1:",
+    df.select(
+        (
+            2
+            ** (
+                pl.col("winners").list.set_intersection(pl.col("selected")).list.len()
+                - 1
+            )
+        ).cast(pl.Int64, strict=False)
+    ).sum(),
+)
+
+print("Part 2:", df_base.select("hit_count").max())
